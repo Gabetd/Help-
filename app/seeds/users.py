024d08ffa445c64ae1a72a14bbdf2a56c6108997
Gabriel <gabetd0904@gmail.com>
2022-12-21 16:17:32 -0600

    error

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,11 +4,11 @@ from app.models import db, User, environment, SCHEMA
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
+        username='Demo', email='demo@aa.io', password='password', profile_image='https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fprofile%2520picture%2F&psig=AOvVaw2NATKDFLuffSjFAL_avSZA&ust=1671747375423000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCJjnhrXei_wCFQAAAAAdAAAAABAE')
     marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
+        username='marnie', email='marnie@aa.io', password='password', profile_image='https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fprofile%2520picture%2F&psig=AOvVaw2NATKDFLuffSjFAL_avSZA&ust=1671747375423000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCJjnhrXei_wCFQAAAAAdAAAAABAE')
     bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+        username='bobbie', email='bobbie@aa.io', password='password', profile_image='https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fprofile%2520picture%2F&psig=AOvVaw2NATKDFLuffSjFAL_avSZA&ust=1671747375423000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCJjnhrXei_wCFQAAAAAdAAAAABAE')
 
     db.session.add(demo)
     db.session.add(marnie)
@@ -27,5 +27,5 @@ def undo_users():
         db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
     else:
         db.session.execute("DELETE FROM users")
-        
+
     db.session.commit()
