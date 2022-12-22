23648dc6ffa000f2e036e2e8d33da9c0bb0f7355
Gabriel <gabetd0904@gmail.com>
2022-12-22 12:27:17 -0600

    yolo

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,5 @@
 from flask.cli import AppGroup
-from .users import seed_users, undo_users
+from .users import seed_users
 from .businesses import seed_businesses
 from .reviews import seed_reviews
 
@@ -18,7 +18,7 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_users()
+        # undo_users()
         # undo_businesses()
         # undo_reviews()
     seed_users()
