cb0ef32446fa3ab58c5d612a86a0f8f0dd2b9fe9
Gabriel <gabetd0904@gmail.com>
2022-12-21 18:48:24 -0600

    because

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .businesses import seed_businesses, undo_businesses
+from .reviews import seed_reviews, undo_reviews
 
 from app.models.db import db, environment, SCHEMA
 
