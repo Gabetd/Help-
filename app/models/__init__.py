c6c690b14c7c8a7ef58d4c78d6ba7d7603c43679
Gabriel <gabetd0904@gmail.com>
2022-12-21 18:06:10 -0600

    please

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,7 @@
 from .db import db
 from .user import User
+from .business import Business
+from .review import Review
+from .review_image import Review_Image
+from .business_image import Business_Image
 from .db import environment, SCHEMA
