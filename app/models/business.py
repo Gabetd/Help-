e7693360b50242ceb88f8ff696e92249279a2261
Gabriel <gabetd0904@gmail.com>
2022-12-22 16:40:42 -0600

    helped

--- app/models/business.py
+++ app/models/business.py
@@ -12,7 +12,7 @@ class Business(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    owner_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False)
     business_name = db.Column(db.String(100), nullable=False)
     phone = db.Column(db.String(12), nullable=False)
     street_address = db.Column(db.String(100), nullable=False)
